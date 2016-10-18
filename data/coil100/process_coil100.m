f9407055e89e1a9325cd11ab3ff560949d55da0f
yingzhenyang <superyyzg@gmail.com>
2016-10-17 20:44:30 -0500

    data

---
+++ data/coil100/process_coil100.m
@@ -0,0 +1,4 @@
+coil100 = load('coil100');
+data = double(coil100.fea);
+tlabel = coil100.gnd;
+clear 'coil100';
---
+++ data/coil20/process_coil20.m
@@ -0,0 +1,4 @@
+coil20 = load('coil20');
+data = coil20.fea;
+tlabel = coil20.gnd;
+clear 'coil20';
